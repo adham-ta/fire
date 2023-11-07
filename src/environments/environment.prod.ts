41e65ff536d65b68127ee0124c372f1c06d92877
adham-ta <69330652+adham-ta@users.noreply.github.com>
2023-11-07 17:37:26 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,10 @@
-export const environment = {
-  production: true,
-  firebase:  {
-    apiKey: 'AIzaSyAKlEX5a8PQaadYNSrohqfl33O37Kjkv1Y',
-    authDomain: 'angular-voxer.firebaseapp.com',
-    databaseURL: 'https://angular-voxer.firebaseio.com',
-    projectId: 'angular-voxer',
-    storageBucket: 'angular-voxer.appspot.com',
-    messagingSenderId: '122050526979',
-    appId: '1:122050526979:web:529304dce5ffca7a'
-  }
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDQannbbK5Dioc2Qk-PSJsub_90z5mezeo",
+  authDomain: "vice-ar.firebaseapp.com",
+  projectId: "vice-ar",
+  storageBucket: "vice-ar.appspot.com",
+  messagingSenderId: "667785819446",
+  appId: "1:667785819446:web:c41e0fb5c2b17cf2b16f97",
+  measurementId: "G-KHJYCEGNDG"
 };
